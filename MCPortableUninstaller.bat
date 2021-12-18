7a47bb42314c2b04bb1e2e90814e143339be786c
Papela <45976195+Papela@users.noreply.github.com>
2021-12-18 13:50:58 +0100

    Update MCPortableUninstaller.bat

--- MCPortableUninstaller.bat
+++ MCPortableUninstaller.bat
@@ -3,13 +3,13 @@ color 4
 title Desinstalando Minecraft...
 cls
 @echo Desinstalando Minecraft...
-cd "C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\"
+cd "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\"
 rename Minecraft Deleted
-del /F /Q Deleted
+rd /s /Q Deleted
 timeout /t 1
 cd "C:\"
 rename PapelaMC TempDeleteMe
 timeout /t 1
-del /F /Q TempDeleteMe
+rd /S /Q TempDeleteMe
 msg * /SERVER:%computername% Minecraft Portable se ha desinstalado correctamente.
 exit
